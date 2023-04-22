508c1247ecb6bca60798015d3bb3db008fb4f397
vpspepe <victor101pepe@hotmail.com>
2023-04-22 14:56:51 -0300

    testa agora japones

--- Entrega_23_04/Reg_Banco.v
+++ Entrega_23_04/Reg_Banco.v
@@ -1,9 +1,9 @@
 module Reg_Banco(
     input[4:0] Ra,   //guarda endereço que será alterado tanto na mem quanto no banco_de_registradore
     input[4:0] Rb,   //guarda endereço que será usado SÓ PARA LER valores
-    input[4:0] Rw,  
-    input WE_Reg,       
-    input[63:0]dIN, 
+    input[4:0] Rw,  //endereço do reg que vair receber o load
+    input WE_Reg,  //write enable     
+    input[63:0]dIN,  //data in
     output reg [63:0] doutA, //valor de leitura do Registrador R
     output reg [63:0] doutB, //valor de leitura do Rb
     input clk
@@ -11,56 +11,58 @@ module Reg_Banco(
 
 //gera os 32 registadores
 
-/*!!*/reg [63:0] banco_registradores[31:0];
+/*!!*/reg [63:0] banco_registradores[31:0]; //ACHO QUE NAO PRECISA DESSES REGS
 /*!!*/wire [63:0] banco_registradores_wire[31:0];
-reg[4:0] loads;
-wire[4:0] loads_wire;
+
+reg[31:0] loads;
+
+ RegNbits Regs( //registrador x0 sempre tem o valor 0
+                .x(64'b0),
+                .clk(clk), 
+                .load(loads[0]), 
+                .x_out(banco_registradores_wire[0]) //saida precisava ser fio, e nao reg
+                );
+
 genvar i;
 generate 
 
-for (i = 0; i < 32 ; i = i+1 ) begin
+for (i = 1; i < 32 ; i = i+1 ) begin
 
     RegNbits Regs(
                 .x(dIN),
                 .clk(clk), 
-                .load(loads_wire[i]), 
-                .x_out(banco_registradores_wire[i])
+                .load(loads[i]), 
+                .x_out(banco_registradores_wire[i]) //saida precisava ser fio, e nao reg
                 );
 end
 endgenerate
 
-integer j,k;
-
-always @* begin
-    for (k = 0; k < 32; k = k + 1) begin
-        banco_registradores[k] <= banco_registradores_wire[k];
-    end
-end
-
-always @* begin
-    for (k = 0; k < 32; k = k + 1) begin
-        loads[k] <= loads_wire[k];
-    end
-end
-
+// acho que esse trecho nao faz sentido, ja que a saida do modulo de regNbits é um fio
 initial begin //inicializa todos os registradores do banco com 0
         for (j = 0; j< 32; j = j + 1) 
             banco_registradores[j] <= 0;
-            loads[j] <= 0;
-    end
+end
 
-//Leitura de dados no banco de registradores (Store)
+integer j,k;
 
-    always @(Ra,Rb) begin  //se Ra ou Rb recebem algum valor, Dout's são as leituras do banco de registradores
-        doutA <= banco_registradores[Ra];
-        doutB <= banco_registradores[Rb];
-    end
+//ACHO QUE ESSE BLOCO ABAIXO EH DESNCECESSÁRIO
+always @* begin //faz com que os registradores sempre sejam definidos como os fios que saem dos modulos regNbits
+    for (k=0; k<32; k= k+1)
+    banco_registradores[k] <= banco_registradores_wire[k];
+end
 
-//Escrita de dados no banco de registradores (load)
+
+
+
+//Load e store
     always @(posedge clk) begin
+        doutA <= banco_registradores_wire[Ra];
+        doutB <= banco_registradores_wire[Rb];
         if (WE_Reg) begin
             loads[Rw] <= 1;
         end
+        else
+            loads[Rw] <=0;
     end
 
 endmodule
