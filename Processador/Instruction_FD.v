d95b10540fe0876cdee36c24189140438afff20f
mtky25 <ma.super03@gmail.com>
2023-05-08 11:03:36 -0300

    instruction

--- Processador/Instruction_FD.v
+++ Processador/Instruction_FD.v
@@ -5,29 +5,25 @@ module Instruction_FD(
     input [1:0] OP_MEM_I,
     input ADD_SUB,
     input PC_load,
-    input[31:0] PC_add,
     output [31:0] instruction_out,
-    output [4:0] Ra_out,Rb_out,Rw_out,
-    output [63:0] offset_out
+    input [5:0] flags
 );
 
     
-    wire[31:0] add_instruction;
+    wire[31:0] addr_instruction;
     wire[31:0] instruction;
     wire [4:0] Ra,Rb,Rw;
-    wire [63:0] offset;
+    wire [31:0] imm;
+    wire [31:0] imm_or_const;
+    wire [31:0] PC_addr;
 
-
-    assign offset = instruction[6:0] == 7'b0110011 ? {5'b00000,instruction[31:25]} : instruction[31:20];
+    assign imm = instruction[6:0] == 7'b0110011 ? {5'b00000,instruction[31:25]} : instruction[31:20];
     assign Ra = instruction[19:15];
     assign Rb = instruction[6:0] == 7'b0110011 ? instruction [24:20] : 0;
     assign Rw = instruction[11:7];
-    assign Ra_out = Ra;
-    assign Rb_out = Rb;
-    assign Rw_out = Rw;
 
 
-    assign offset_out = offset;
+    assign imm_out = imm;
     assign instruction_out = instruction;
     
 FD FD(
@@ -36,23 +32,25 @@ FD FD(
     .Rw(Rw),  
     .WE_reg(WE_reg), .WE_mem(WE_mem),       
     .clk(clk),
-    .OFFSET(offset),
+    .OFFSET(imm),
     .OP_MEM_I(OP_MEM_I),
     .ADD_SUB(ADD_SUB)
 );
 
 Reg32 PC(
     .clk(clk),
-    .x(PC_add), 
+    .x(PC_addr), 
     .load(PC_load),
-    .x_out(add_instruction)
+    .x_out(addr_instruction)
 );
 
 MemInstruction mem_instruction(
-    .instruction(add_instruction),
+    .instruction(addr_instruction),
     .dout(instruction)
 );
 
+MUX2_32 MUX2(.a(1),.b(imm),.select(flags),.result(imm_or_const));
 
+adder add(.a(imm_or_const),.b(addr_instruction),.result(PC_addr));
 
 endmodule
